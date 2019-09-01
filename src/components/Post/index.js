d888da6f276bc3eb59c054f564793c9b36233277
Ekaterina Gulevich <meteorgul@gmail.com>
2019-09-01 17:35:33 +0300

    добавляет компонент Post для отображения краткой записи поста #pmb-4

---
+++ src/components/Post/index.js
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+import "./style.scss";
+import CG from "home/utils/CG";
+
+/** Компонент для отображения поста с кратким описанием */
+
+class Posts extends Component {
+  static get propTypes() {
+    return {
+      /**название поста */
+      name: PropTypes.string.isRequired,
+      /**описание поста */
+      description: PropTypes.string.isRequired,
+      /**количество комментариев */
+      count: PropTypes.number.isRequired,
+      /**ссылка на страницу поста */
+      link: PropTypes.string.isRequired
+    };
+  }
+  static get defaultProps() {
+    return {
+      name: "",
+      description: "",
+      count: 0
+    };
+  }
+  constructor(props) {
+    super(props);
+
+    this.CG = CG.bind(this, "post");
+  }
+
+  render() {
+    return (
+      <div className={this.CG()}>
+        <div className={this.CG("title")}>{this.props.name}</div>
+        <div className={this.CG("description")}>{this.props.description}</div>
+        <div className={this.CG("sub-text")}>
+          <div className={this.CG("comment-count")}>
+            Комментариев: {this.props.count}
+          </div>
+          <Link className={this.CG("link")} to={this.props.link}>
+            Перейти
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Posts;

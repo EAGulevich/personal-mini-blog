b5ea4fc9dd53a5275dabf889e513aca829ab3c51
Ekaterina Gulevich <meteorgul@gmail.com>
2020-01-27 17:49:35 +0300

    стилизует страницу 404

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,13 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import "./style.scss";
+import CG from "home/utils/CG";
+import Header from "home/components/Header";
 
 /** Заглушка, если страница не найдена */
 
-class NotFound extends Component {
-  render() {
-    return <h1>Страница не найдена</h1>;
-  }
+function NotFound(props) {
+  const [isShowMenu, setIsShowMenu] = useState(false);
+
+  return (
+    <div className={CG("not-found", "", [isShowMenu ? "with-menu" : ""])}>
+      <Header title={"Тупичок"} />
+      <h1>404</h1>
+      <h2>Вы все сломали...</h2>
+      {!isShowMenu && (
+        <button onClick={setIsShowMenu.bind(null, true)}>Показать меню</button>
+      )}
+      <img src="img/404.jpg" />
+    </div>
+  );
 }
 
 export default NotFound;
